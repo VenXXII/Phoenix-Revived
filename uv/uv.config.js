e30b3b8cf9fafa3dd09bb6b445148fcddd56654e
Ven <gamerma223@gmail.com>
2022-11-29 19:53:04 -0500

    Initial commit

---
+++ uv/uv.config.js
@@ -0,0 +1,11 @@
+/*global Ultraviolet*/
+self.__uv$config = {
+    prefix: '/service/',
+    bare: 'https://uv.holyubofficial.net/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv.handler.js',
+    bundle: '/uv.bundle.js',
+    config: '/uv.config.js',
+    sw: '/uv.sw.js',
+};
